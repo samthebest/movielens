dfa6feb797898815a4bc9847fa8205f102f81d9d
sam.savage <foo@bar.com>
2018-10-19 17:29:19 +0100

    NOJIRA Wrote topMovies

--- src/main/scala/movielens/Stats.scala
+++ src/main/scala/movielens/Stats.scala
@@ -2,21 +2,41 @@ package movielens
 
 import org.apache.spark.rdd.RDD
 
-case class CountAndAverageRating(movieCount: Int, ratingTotal: Int)
+case class CountAndRatingTotal(movieCount: Int, ratingTotal: Int)
 
 object Stats {
+  def addCountAndRatingTotal(left: CountAndRatingTotal, right: CountAndRatingTotal): CountAndRatingTotal =
+    CountAndRatingTotal(left.movieCount + right.movieCount, left.ratingTotal + right.ratingTotal)
+
   def ratingsToUserStats(ratings: RDD[Rating]): RDD[UserStats] =
-    ratings.map(rating => rating.userID -> CountAndAverageRating(1, rating.rating))
-    .reduceByKey {
-      case (left, right) =>
-        CountAndAverageRating(left.movieCount + right.movieCount, left.ratingTotal + right.ratingTotal)
-    }
+    ratings.map(rating => rating.userID -> CountAndRatingTotal(1, rating.rating))
+    .reduceByKey(addCountAndRatingTotal)
     .map {
-      case (userId, CountAndAverageRating(movieCount, ratingTotal)) =>
+      case (userId, CountAndRatingTotal(movieCount, ratingTotal)) =>
         UserStats(userId, movieCount, ratingTotal.toDouble / movieCount)
     }
 
-  def moviesToGenreCounts(movies: RDD[Movie]): RDD[GenreCount] = {
-    ???
+  def moviesToGenreCounts(movies: RDD[Movie]): RDD[GenreCount] =
+    movies.flatMap(_.genres).map((_, 1L)).reduceByKey(_ + _).map {
+      case (genre, count) => GenreCount(genre, count)
+    }
+
+  // Observe we avoid a join here, but we assume `limit` is not very large. Considerably different algorithm required for
+  // large `limit`
+  def topMovies(movies: RDD[Movie], ratings: RDD[Rating], limit: Int): RDD[MovieRank] = {
+    val ratingsMap: Map[Int, (Double, Int)] =
+      ratings.map(rating => rating.movieID -> CountAndRatingTotal(1, rating.rating))
+      .reduceByKey(addCountAndRatingTotal)
+      .mapValues(countAndRatingTotal => countAndRatingTotal.ratingTotal.toDouble / countAndRatingTotal.movieCount)
+      .top(limit)(Ordering.by[(Int, Double), Double](_._2))
+      .zipWithIndex.map {
+        case ((movieID, averageRating), rank) => movieID ->(averageRating, rank + 1)
+      }
+      .toMap
+
+    movies.flatMap(movie => ratingsMap.get(movie.movieID).map {
+      case (averageRating, rank) => MovieRank(rank, movie.movieID, movie.title, averageRating)
+    })
+    .sortBy(_.rank)
   }
 }
--- src/test/scala/movielens/StatsTest.scala
+++ src/test/scala/movielens/StatsTest.scala
@@ -4,24 +4,40 @@ import org.specs2.mutable.Specification
 import StaticSparkContext._
 
 object StatsTest extends Specification {
+  val ratings = sc.makeRDD(Seq(
+    Rating(userID = 1, movieID = 1, rating = 5),
+    Rating(userID = 1, movieID = 2, rating = 3),
+    Rating(userID = 1, movieID = 3, rating = 4),
+    Rating(userID = 3, movieID = 1, rating = 5),
+    Rating(userID = 4, movieID = 1, rating = 5),
+    Rating(userID = 4, movieID = 4, rating = 2),
+    Rating(userID = 2, movieID = 3, rating = 5),
+    Rating(userID = 2, movieID = 2, rating = 5),
+    Rating(userID = 2, movieID = 1, rating = 5)
+  ))
+
+//  Rating(userID = 1, movieID = 2, rating = 3),
+//  Rating(userID = 2, movieID = 2, rating = 5),
+
+//  (5 + 3) / 2 = 4.0
+
+  val movies = sc.makeRDD(Seq(
+    Movie(1, "1t", List("a", "b")),
+    Movie(2, "2t", List("a", "b", "c")),
+    Movie(3, "3t", List("b")),
+    Movie(4, "4t", List("a")),
+    Movie(5, "5t", List("a")),
+    Movie(6, "6t", List("d", "e"))
+  ))
+
   "Stats.ratingsToUserStats" should {
     "Take an RDD of Ratings and return an RDD of UserStats, i.e. a list of users with no of movies they rated " +
       "and average rating per user" in {
-      Stats.ratingsToUserStats(sc.makeRDD(Seq(
-        Rating(userID = 1, movieID = 1, rating = 5),
-        Rating(userID = 1, movieID = 2, rating = 3),
-        Rating(userID = 1, movieID = 3, rating = 4),
-        Rating(userID = 3, movieID = 1, rating = 5),
-        Rating(userID = 4, movieID = 1, rating = 5),
-        Rating(userID = 4, movieID = 4, rating = 4),
-        Rating(userID = 2, movieID = 3, rating = 5),
-        Rating(userID = 2, movieID = 2, rating = 5),
-        Rating(userID = 2, movieID = 1, rating = 5)
-      ))).collect().toList.sortBy(_.userID) must_=== List(
+      Stats.ratingsToUserStats(ratings).collect().toList.sortBy(_.userID) must_=== List(
         UserStats(userID = 1, numberOfMovies = 3, averageRating = 4.0),
         UserStats(userID = 2, numberOfMovies = 3, averageRating = 5.0),
         UserStats(userID = 3, numberOfMovies = 1, averageRating = 5.0),
-        UserStats(userID = 4, numberOfMovies = 2, averageRating = 4.5)
+        UserStats(userID = 4, numberOfMovies = 2, averageRating = 3.5)
       )
     }
   }
@@ -29,19 +45,23 @@ object StatsTest extends Specification {
   "Stats.moviesToGenreCounts" should {
     "Take an RDD of Movies and return an RDD of GenreCounts, i.e. list of unique Genres and no of movies " +
       "under each genre" in {
-      Stats.moviesToGenreCounts(sc.makeRDD(Seq(
-        Movie(1, "", List("a", "b")),
-        Movie(2, "", List("a", "b", "c")),
-        Movie(3, "", List("b")),
-        Movie(4, "", List("a")),
-        Movie(5, "", List("a")),
-        Movie(5, "", List("d", "e"))
-      ))).collect().toList.sortBy(_.genre) must_=== List(
+      Stats.moviesToGenreCounts(movies).collect().toList.sortBy(_.genre) must_=== List(
         GenreCount("a", 4),
         GenreCount("b", 3),
         GenreCount("c", 1),
         GenreCount("d", 1),
         GenreCount("e", 1)
+      )
+    }
+  }
+
+  "Stats.topMovies" should {
+    "Take an RDD of Movies and an RDD of Rating and return a RDD of MovieRank where the RDD is sorted by the rank" +
+      " (1 being highest). Also only return N results" in {
+      Stats.topMovies(movies, ratings, 3).collect().toList must_=== List(
+        MovieRank(rank = 1, movieID = 1, title = "1t", averageRating = 5.0),
+        MovieRank(rank = 2, movieID = 3, title = "3t", averageRating = 4.5),
+        MovieRank(rank = 3, movieID = 2, title = "2t", averageRating = 4.0)
       )
     }
   }
